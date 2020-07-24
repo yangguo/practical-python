be6a01e57b3f02362219eb84045a0f328b7e84f3
Vincent Yang <yf2005@gmail.com>
2020-07-24 09:38:57 +0800

    file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+Totalcost = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        row = line.split(',')
+        # print(row)
+        Totalcost+=int(row[1])*float(row[2])
+
+print('Total cost ',Totalcost)
